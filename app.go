378f1ac7d01707914f37c7e29057fad46b6f03f5
John Higgins <johnmatthiggins@gmail.com>
2024-02-11 00:51:31 -0800

    added json parsing

--- app.go
+++ app.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -12,6 +14,12 @@ import (
 
 const USERNAME = "johnmatthiggins"
 const GITHUB_URL = "api.github.com"
+
+type RepositoryData struct {
+	Id       uint64
+	Name     string
+	FullName string
+}
 
 func main() {
 	err := godotenv.Load()
@@ -25,16 +33,52 @@ func main() {
 	// Get list of public repositories...
 	// Fetch commits from each repository...
 	// Save all the information to database...
-	var reposEndpoint = path.Join(GITHUB_URL, fmt.Sprintf("users/%s", USERNAME))
+	var reposEndpoint = path.Join(GITHUB_URL, "user/repos")
 
 	client := &http.Client{}
-	request, err := http.NewRequest("GET", fmt.Sprintf("https://%s?per_page=100&type=owner", reposEndpoint), nil)
-	request.Header.Add("Bearer", token)
+	request, err := http.NewRequest("GET", fmt.Sprintf("https://%s?per_page=100&type=public", reposEndpoint), nil)
+	request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	request.Header.Add("Accept", "application/vnd.github+json")
+	request.Header.Add("X-GitHub-Api-Version", "2022-11-28")
 
 	response, err := client.Do(request)
 	if err != nil {
 		log.Fatal(err)
 	} else {
-		fmt.Println(response)
+		// data, err := io.ReadAll(response.Body)
+
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+
+		// fmt.Println(string(data))
+
+		var repositories []interface{}
+		bytes, err := io.ReadAll(response.Body)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = json.Unmarshal([]byte(bytes), &repositories)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var repositoryData []RepositoryData
+
+		for _, repository := range repositories {
+			id, _ := repository.(map[string]interface{})["id"].(uint64)
+			name, _ := repository.(map[string]interface{})["name"].(string)
+			fullName, _ := repository.(map[string]interface{})["full_name"].(string)
+			repo := RepositoryData{
+				Id:       id,
+				Name:     name,
+				FullName: fullName,
+			}
+
+			repositoryData = append(repositoryData, repo)
+		}
+
+		fmt.Println(repositoryData)
 	}
 }

c42cfef0cee5cd7e519cd80d2d59553b8e1002a0
John Higgins <johnmatthiggins@gmail.com>
2024-02-11 15:09:17 -0800

    got all commits from public repos parsed

--- app.go
+++ app.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -21,6 +22,11 @@ type RepositoryData struct {
 	FullName string
 }
 
+type CommitData struct {
+	Hash string
+	Date time.Time
+}
+
 func main() {
 	err := godotenv.Load()
 
@@ -29,8 +35,33 @@ func main() {
 	}
 
 	token := os.Getenv("GITHUB_API_TOKEN")
-	repositories, err := getRepos(token)
-	fmt.Println(repositories)
+	commits, err := getAllCommits(token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(commits)
+}
+
+func getAllCommits(token string) ([]CommitData, error) {
+	repos, err := getRepos(token)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var commits []CommitData
+
+	for _, repo := range repos {
+		repoCommits, err := getCommitsFromRepo(repo.FullName, token)
+		if err != nil {
+			return nil, err
+		}
+
+		commits = append(commits, repoCommits...)
+	}
+
+	return commits, nil
 }
 
 func getRepos(token string) ([]RepositoryData, error) {
@@ -81,4 +112,65 @@ func getRepos(token string) ([]RepositoryData, error) {
 	}
 
 	return nil, err
+}
+
+// Full name is {owner}/{slug}
+// Example: johnmatthiggins/git-commit-scraper
+func getCommitsFromRepo(fullName string, token string) ([]CommitData, error) {
+	dayDuration, _ := time.ParseDuration("24h")
+	var endpoint string = fmt.Sprintf("https://%s", path.Join(GITHUB_URL, "repos", fullName, "commits"))
+	var fiftyTwoWeeksAgo = time.Now().Add(-dayDuration * 52 * 7)
+	var startISOTime = fiftyTwoWeeksAgo.Format(time.RFC3339)
+	var url = fmt.Sprintf("%s?committer=johnmatthiggins&since=%s", endpoint, startISOTime)
+
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	request.Header.Add("Accept", "application/vnd.github+json")
+	request.Header.Add("X-GitHub-Api-Version", "2022-11-28")
+
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	var commits []interface{}
+	bytes, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal([]byte(bytes), &commits)
+	if err != nil {
+		return nil, err
+	}
+
+	var commitData []CommitData
+
+	for _, commit := range commits {
+		// commit -> author -> date
+		hash, _ := commit.(map[string]interface{})["sha"].(string)
+
+		// getting commit time
+		innerCommit, _ := commit.(map[string]interface{})["commit"]
+		commitAuthor, _ := innerCommit.(map[string]interface{})["author"]
+		commitTimeStr, _ := commitAuthor.(map[string]interface{})["date"].(string)
+		commitTime, err := time.Parse(time.RFC3339, commitTimeStr)
+
+		if err != nil {
+			return nil, err
+		}
+
+		newCommitData := CommitData{
+			Hash: hash,
+			Date: commitTime,
+		}
+
+		commitData = append(commitData, newCommitData)
+	}
+
+	return commitData, nil
 }
